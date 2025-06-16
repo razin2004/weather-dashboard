d8b64b8117154139690b11452988063feb73e0dc
razin2004 <razinmuhammed1999@gmail.com>
2025-06-16 18:57:27 +0530

    app.py

--- app.py
+++ app.py
@@ -45,4 +45,6 @@ def data():
     return jsonify({'labels': labels, 'temps': temps})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
